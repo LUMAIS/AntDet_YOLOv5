5c7bcbba819a347804ccfd5bbf4ed7eab7ea0425
PikachuDeveloper <shevacharmeleon@gmail.com>
2021-09-06 13:06:36 +0300

    added new files for labeling

--- training/lbxTorch.py
+++ training/lbxTorch.py
@@ -1,7 +1,6 @@
-import os
-import numpy as np
 import json
-from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+import os
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter, SUPPRESS
 
 # Dictionary that maps class names to IDs
 class_name_to_id_mapping = {"ant": 0,
@@ -11,15 +10,16 @@ class_name_to_id_mapping = {"ant": 0,
                             "trophallaxis-larva": 4,
                             "food-noise": 5,
                             "pupa": 6,
-                            "barcode": 7,
-                            "uncategorized": 8}
+                            "barcode": 7} #,"uncategorized": 8}
+# we delete uncategorized from possible classes to simplify the process of omitting them
 
 
 # Convert the frame dict to the required yolo format and write it to disk
-def convert_to_yolo(jsfile, img_size, outdir):
+def convert_to_yolo(jsfile, img_size, filename, outdir):
     """
     jsfile: list from loaded json file
-    img_size: tuple = (height, width) of the image/frame
+    img_size: tuple = (width, height) of the image/frame
+    filename: future name of each txt file will take it as a beginning
     outdir: output directory for saving txt files
     """
     # go to an output directory
@@ -27,9 +27,6 @@ def convert_to_yolo(jsfile, img_size, outdir):
         os.makedirs(outdir)
     cwd = os.getcwd()
     os.chdir(os.path.join(cwd, outdir))
-
-    # we delete uncategorized from possible classes to simplify the process of omitting them
-    class_name_to_id_mapping.pop("uncategorized")
 
     for frame in jsfile:
         print_buffer = []
@@ -52,7 +49,7 @@ def convert_to_yolo(jsfile, img_size, outdir):
                     b_height = b["height"]
 
                     # Normalise the coordinates by the dimensions of the image
-                    image_h, image_w = img_size
+                    image_w, image_h = img_size
                     b_center_x /= image_w
                     b_center_y /= image_h
                     b_width /= image_w
@@ -70,13 +67,14 @@ def convert_to_yolo(jsfile, img_size, outdir):
 
         framenum = str(frame["frameNumber"])
         # Save the annotation to disk
-        print("\n".join(print_buffer), file=open(framenum + ".txt", "w"))
+        print("\n".join(print_buffer), file=open('{}_{}.txt'.format(filename, framenum), "w"))
 
 #counts number of modified objects on frames, which were listed in the keyframes
-def count_salary(jsfile, keyframes):
+def count_objects(jsfile, keyframes, obj_cost):
     """
     jsfile: list from loaded json file
     keyframes: string <n1>-<n2>,<n3>-<n4>,<n5>...
+    obj_cost: cost of 1 annotation
     :return: number of modified objects
     """
 
@@ -116,31 +114,39 @@ def count_salary(jsfile, keyframes):
     print("Frames taken to account: ", print_buffer)
     for key, value in cls_count.items():
         print("{}: {}".format(key, value))
-    print("Total: {} \nSalary: {}$".format(sum, sum*0.03))
+    print("Total: {} \nCost: ${}".format(sum, sum*obj_cost))
     return sum
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='Document Taxonomy Builder.',
                             formatter_class=ArgumentDefaultsHelpFormatter,
                             conflict_handler='resolve')
-    parser.add_argument('-json-path', '--filepath', default="EuresysCapturing_IR_100_2021-08-24_17.json",
-                        help='Path for json file')
+    parser.add_argument('-json-path', '--filepath', nargs='+',
+                        help='Path for json files')
     # parser.add_argument('-vid', '--vid-path', default=None,
     #                     help='Path for the video')
-    parser.add_argument('-o', '--outp-dir', default='labels',
+
+    #create group with mutually exclusive elements: framesize and keyframe-obj
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-s', '--frame-size', default=None, type=str,
+                       help='The size format is WxH, for example: 800x600')
+    parser.add_argument('-o', '--outp-dir', type = str,
+                        default=os.path.join(os.getcwd(), 'labels'),
                         help='Output directory for the label files')
-    parser.add_argument('-k', '--keyframed-objects', default=None, type=str,
-                        help='String, that specify frames, where number of changed points should be counted')
+
+    parser.add_argument('--object-cost', help=SUPPRESS, default=0.03, type=float)
+    group.add_argument('-k', '--keyframed-objects', default=None, type=str,
+                       help='String, that specify range of frames to count modified objects')
 
     args = parser.parse_args()
 
-    #if args.keyframed_objects and not findall(pattern, args.keyframed_objects):
-        # print("Something wrong with your frame list. Check if it looks like f1-f2,f3-f4,...,f(n)-f(n+1) or just separeted frames")
-    with open(args.filepath) as jsonFile:
-        annotations = json.load(jsonFile)
-        jsonFile.close()
-    img_size = (1000, 1000)
-    if args.keyframed_objects:
-        count_salary(annotations, args.keyframed_objects)
-    else:
-        convert_to_yolo(annotations, img_size, args.outp_dir)
+    for filepath in args.filepath:
+        with open(filepath) as jsonFile:
+            annotations = json.load(jsonFile)
+
+        if args.keyframed_objects:
+            count_objects(annotations, args.keyframed_objects, args.object_cost)
+        else:
+            fm_size = tuple(map(lambda y: int(y), args.frame_size.split('x')))
+            filename = os.path.split(filepath)[1][:-5]
+            convert_to_yolo(annotations, fm_size, filename, args.outp_dir)
