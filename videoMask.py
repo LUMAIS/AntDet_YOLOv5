969552f06c25c6980c77b70e460facf3a5e8fb5f
PikachuDeveloper <shevacharmeleon@gmail.com>
2021-11-10 18:50:53 +0200

    Bug of rectangle video fixed. Added random pixel coloring for background.

--- videoMask.py
+++ videoMask.py
@@ -1,83 +1,99 @@
-from re import findall
-from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import cv2
 import numpy as np
-from PIL import Image, ImageDraw
 import matplotlib._color_data as mcd
 
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+from re import split
+from PIL import Image, ImageDraw
+from numpy.random import randint
 
-def roi_processing(vidpath, color, rois, filename):
+
+class Roi:
+    """
+    Created to process and keep relevant fields in it.
+
+    Fields:
+        xywh (tuple) - Top Left Width Height
+        shape(str) - ellipse or rect
+        start(int) - first frame to draw ROI on
+        end (int, str) -  last frame to draw ROI on
+    """
+
+    def __init__(self, roi):
+        self.xywh = None
+        self.shape = None
+        self.start = 1
+        self.end = '$'
+        self.__create(roi)
+
+    def __create(self, roi):
+        params = split(r',|;|\^|!', roi)
+        self.xywh = tuple(map(int, params[:4]))
+        self.shape = "ellipse" if "ellipse" in roi else "rect"
+        params = split(r'ellipse|rect|\^|!', roi)[1:]
+        params = [i for i in params if i != '']
+        params = list(map(int, params))
+
+        self.start = self.start if "^" not in roi else params[0]
+        self.end = self.end if "!" not in roi else params[-1]
+
+
+def roi_processing(vidpath: str, rois: str, filename: str, rand: bool = False, color: str = "black"):
     """
 
     Args:
         vidpath (str): path to data with filename
-        color (str): color of a background written in English (red, blue, etc.)
         rois (str): list of strings LEFT,TOP,WIDTH,HEIGHT [;SHAPE=rect][^FRAME_START=0][!FRAME_FINISH=LAST_FRAME]]
         filename (str): name for a new video
-    Returns:
-        0 if process ended with errors
+        rand (bool): True if background needs to be randomly colored
+        color (str): color of a background written in English (red, blue, etc.)
     """
     vid = cv2.VideoCapture(vidpath)
-    total = int(vid.get(cv2.CAP_PROP_FRAME_COUNT)) #number of frames in a video
+    total = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))  # number of frames in a video
     _, frame = vid.read()
+    height, width = frame.shape[:2]
 
-    # ndarray of masks for each frame
-    masks = [Image.new("L", frame.shape[:2], 0) for i in range(total)]
-
+    roi_list = []
     for roi in rois:
-        if not findall(r'\d+,\d+,\d+,\d+', roi):
-            print("Process interrupted \nInvalidArgument: wrong number of arguments in " + roi)
-            return 0
+        roi = Roi(roi)
+        roi.end = total if roi.end == "$" else roi.end
+        roi_list.append(roi)
 
-        params = findall(r'\d+', roi)
-        if len(params) < 4 or len(params) > 6:
-            print("Process interrupted \nInvalidArgument: wrong number of arguments in " + roi)
-            return 0
-
-        x1, y1, w, h = list(map(int, params[:4]))
-        if x1 > frame.shape[1] or y1 > frame.shape[0]:
-            print("Process interrupted \nInvalidArgument: wrong coordinates of the roi specified as " + roi)
-            return 0
-        # get Left, Top, Width, Height and convert to int
-        # separate xywh from frame intervals which should be processed
-        x2, y2 = min(w + x1, frame.shape[1]), min(h + y1, frame.shape[0])
-
-        if len(params) == 6:
-            numframes = list(map(int, params[4:]))
-        else:
-            if "^" in roi:
-                numframes = list(map(int, params[4:])) + [total]
-            elif "!" in roi:
-                numframes = [1] + list(map(int, params[4:]))
-            else:
-                numframes = [1, total]
-
-        for i in range(numframes[0] - 1, numframes[1]):
-            mask_im = masks[i]
-            draw = ImageDraw.Draw(mask_im)
-            if "ellipse" in roi:
-                draw.ellipse((x1, y1, x2, y2), fill=255)
-            else:
-                draw.rectangle((x1, y1, x2, y2), fill=255)
-
-    vid = cv2.VideoCapture(vidpath)
     writer = cv2.VideoWriter(filename, cv2.VideoWriter_fourcc(*'mp4v'),
-                             vid.get(cv2.CAP_PROP_FPS), frame.shape[:2])
+                             vid.get(cv2.CAP_PROP_FPS), Image.fromarray(frame).size)
 
-    h = mcd.CSS4_COLORS[color] #get hex code for a background color by name
-    bg = Image.new('RGB', frame.shape[:2], h)
-    for mask in masks:
-        _, frame = vid.read(cv2.IMREAD_UNCHANGED)
-        if mask == Image.new("L", frame.shape[:2], 0):
+    hcode = (0, 0, 0) if not color else mcd.CSS4_COLORS[color]  # get hex code for a background color by name
+    bg = Image.new('RGB', (width, height), hcode)
+    for i in range(1, total + 1):
+        mask = Image.new("L", (width, height), 0)
+        for roi in roi_list:
+            if roi.start <= i and i <= roi.end:
+                x1, y1, w, h = roi.xywh
+
+                if x1 > width or y1 > height:
+                    raise UnboundLocalError("Process interrupted \nInvalidArgument: wrong coordinates of the roi")
+
+                # change coordinates of the ROI's Right Bottom corner if it is out of boundaries
+                x2, y2 = min(w + x1, width), min(h + y1, height)
+
+                draw = ImageDraw.Draw(mask)
+                if roi.shape == "ellipse":
+                    draw.ellipse((x1, y1, x2, y2), fill=255)
+                else:
+                    draw.rectangle((x1, y1, x2, y2), fill=255)
+        if mask == Image.new("L", (width, height), 0):
             masked = frame
         else:
-            masked = bg.copy()
-            masked.paste(Image.fromarray(frame), (0, 0), mask)
-            masked = np.array(masked)[:, :, ::-1]
+            im_frame = Image.fromarray(frame[:, :, ::-1])  # RGB
+            if rand:
+                bg = Image.fromarray(randint(0, 256, (height, width, 3)).astype(np.uint8))
+            masked = Image.composite(im_frame, bg, mask)
+            masked = np.array(masked)[:, :, ::-1]  # BGR
         writer.write(masked)
+        _, frame = vid.read()
+    writer.release()
     vid.release()
 
-    return 1
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='Document Taxonomy Builder.',
@@ -85,11 +101,13 @@ if __name__ == '__main__':
                             conflict_handler='resolve')
     parser.add_argument('-v', '--vidpath', type=str, help='path to video')
 
-    parser.add_argument('-c', '--color', type=str, default="black",
-                        help='color written as a word like pink, aqua, etc.')
     parser.add_argument('-r', '--rois', type=str, default=[], action='append',
                         help='LEFT,TOP,WIDTH,HEIGHT[;SHAPE=rect][^FRAME_START=1][!FRAME_FINISH=LAST_FRAME]]')
     parser.add_argument('-f', '--filename', type=str, help='name for a processed video')
-    opt = parser.parse_args()#'-v E:\\100testimages.mp4 -c orange -r 700,800,300,200;ellipse!20 -r 500,400,320,240^18!28 -f new.mp4'.split())
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-c', '--color', type=str, help='color written as a word like pink, aqua, etc.')
+    group.add_argument('-rand', help='True if background needs to be randomly colored')
+    opt = parser.parse_args()
 
     roi_processing(**vars(opt))
