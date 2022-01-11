2953a86eb4fbb690bbac5a6f1b0350c66027690e
PikachuDeveloper <shevacharmeleon@gmail.com>
2022-01-11 12:50:20 +0200

    evaluation of attribute difference

--- visAnnotDiff.py
+++ visAnnotDiff.py
@@ -78,10 +78,10 @@ def visualize_bbox(image: np.ndarray, tool: Dict[str, Any], thickness: int = 2, 
     Returns:
         image with a bounding box drawn on it.
     """
-    start = (int(tool.bbox["left"]), int(tool.bbox["top"]))
-    end = (int(tool.bbox["left"] + tool.bbox["width"]),
-           int(tool.bbox["top"] + tool.bbox["height"]))
-    h = tool.color.lstrip('#')
+    start = (int(tool['bbox']["left"]), int(tool['bbox']["top"]))
+    end = (int(tool['bbox']["left"] + tool['bbox']["width"]),
+           int(tool['bbox']["top"] + tool['bbox']["height"]))
+    h = tool['color'].lstrip('#')
     color = tuple(int(h[i:i + 2], 16) for i in (4, 2, 0)) #BGR
     if style:
         dashrect(image, start, end, color, thickness, style)
@@ -89,7 +89,7 @@ def visualize_bbox(image: np.ndarray, tool: Dict[str, Any], thickness: int = 2, 
         cv2.rectangle(image, start, end, color, thickness)
     return image
 
-
+# no need
 def shorten_file(jsFile: str) -> Dict[str, list]:
     """
     Gets only relevant information from annotation file
@@ -110,6 +110,24 @@ def shorten_file(jsFile: str) -> Dict[str, list]:
             annotDict[frameNum][feature_id] = Annotation(obj['bbox'], obj['color'], obj['keyframe'])
     return annotDict
 
+def get_attr(obj: Dict) -> Dict:
+    """
+    Helps to track changes in attributes over time.
+
+    Args:
+        obj (dict): the piece of the labelbox annotation which describes only 1 object.
+    Return:
+        Shorten dictionary, where key is the attribute and value is a keyframe.
+    """
+    attrs = dict()
+    if obj['classifications']:
+        for classif in obj['classifications']:
+            if classif['answers']:
+                for answer in classif['answers']:
+                    attrs[answer['value']] = answer['keyframe']
+    return attrs
+
+
 
 def main(annotated: str, reviewed: str, video: str, scale: float = 2, vidreview: str = None, keyframes: str = '1-$', epsilon: float = 0):
     """
@@ -120,6 +138,11 @@ def main(annotated: str, reviewed: str, video: str, scale: float = 2, vidreview:
         reviewed (str): path to annotation file after corrections
         video (str): path to data with filename
         keyframes (str): intervals of frames that should be taken into account
+    Return:
+        numclschanges (int) - number of changes in total (among classes such as ant, ant-head, etc.)
+        numattrchanges (int) - number of changes in total (among attributes such as blurry, side-view, etc.)
+        numcorcls (int) - number of changes made by the reviewer (among classes)
+        numcorattr (int) - number of changes made by the reviewer (among attributes)
     """
     with open(annotated) as f1:
         orFile = load(f1)
@@ -130,21 +153,26 @@ def main(annotated: str, reviewed: str, video: str, scale: float = 2, vidreview:
 
     totalel = count_objects(orFile, keyframes, 0)
     total = len(orFile)
-    print("Number of annotated objects is", totalel)
-    orFile = shorten_file(orFile)
-    revFile = shorten_file(revFile)
+    print("------------Total-------------")
+    print(f"Annotated classes: {totalel[0]}\nAnnotated attributes: {totalel[1]}".format(totalel))
+    # orFile = shorten_file(orFile)
+    # revFile = shorten_file(revFile)
 
     writer = None
     if video:
         vid = cv2.VideoCapture(video)
         if vidreview is not None:
-            _, frame = vid.read()
-            height, width = frame.shape[:2]
+            _, img = vid.read()
+            height, width = img.shape[:2]
             writer = cv2.VideoWriter(vidreview, cv2.VideoWriter_fourcc(*'mp4v'),
                                      vid.get(cv2.CAP_PROP_FPS), (width, height))
+    # changes in total made by both AI and in hand
+    numclschanges = 0
+    numattrchanges = 0
+    # changes in made especially by the reviewer
+    numcorcls = 0
+    numcorattr = 0
 
-    numchanges = 0
-    numcorpos = 0
     framelst = strparse(keyframes)
 
     for [beginning, ending] in framelst:
@@ -152,36 +180,54 @@ def main(annotated: str, reviewed: str, video: str, scale: float = 2, vidreview:
 
         if video:
             vid.set(cv2.CAP_PROP_POS_FRAMES, int(beginning))
-            _, frame = vid.read()
+            _, img = vid.read()
         else:
-            frame = np.zeros((1, 1), np.uint8)
+            img = np.zeros((1, 1), np.uint8)
 
         for frameNum in range(int(beginning), int(ending) + 1):
             show = False
-            for feature_id, revobject in revFile[frameNum].items():
-                try:
-                    orBbox = orFile[frameNum][feature_id].bbox
-                    mistake = [abs(orBbox[key] - revobject.bbox[key]) <= epsilon for key in revobject.bbox]
-                    if mistake != [1] * 4:
-                        numchanges += 1
-                        show = video
-                        frame = visualize_bbox(frame, orFile[frameNum][feature_id])
-                        frame = visualize_bbox(frame, revFile[frameNum][feature_id], style='dotted')
-                        if revobject.keyframe:
-                            numcorpos += 1
-                except KeyError:
-                    numcorpos += 1
-                    numchanges += 1
+            rFrame = revFile[frameNum - 1]
+            oFrame = orFile[frameNum - 1]
+
+            for rObj in rFrame['objects']:
+                flag = False # to track down if it's not a new object
+                for oObj in oFrame['objects']:
+                    if rObj['featureId'] == oObj['featureId']:
+                        mistake = [abs(rObj['bbox'][dim] - oObj['bbox'][dim]) <= epsilon for dim in rObj['bbox'].keys()]
+                        if mistake != [1] * 4:
+                            numclschanges += 1
+                            show = video
+                            img = visualize_bbox(img, oObj)
+                            img = visualize_bbox(img, rObj, style='dotted')
+                            if rObj['keyframe']:
+                                numcorcls += 1
+                        flag = True
+                        rAtr = get_attr(rObj)
+                        oAtr = get_attr(oObj)
+                        for attr, keyframe in rAtr.items():
+                            if attr not in oAtr:
+                                if keyframe:
+                                    numcorattr += 1
+                                numattrchanges += 1
+                            # elif keyframe and not oAtr[attr]:
+                            #     numcorattr += 1
+                            #     numattrchanges += 1
+                            # elif keyframe != oAtr[attr]:
+                            #     numattrchanges += 1
+
+                if not flag:
+                    numcorcls += 1
+                    numclschanges += 1
                     show = video
-                    frame = visualize_bbox(frame, revobject, style='dashed')
+                    img = visualize_bbox(img, rObj, style='dashed')
             if writer is not None:
-                writer.write(frame)
+                writer.write(img)
             elif show:
                 wTitle = 'frameNumber ' + str(frameNum)
                 cv2.namedWindow(wTitle, cv2.WINDOW_NORMAL)
-                h, w = frame.shape[:2]
+                h, w = img.shape[:2]
                 cv2.resizeWindow(wTitle, int(w / scale), int(h / scale))
-                cv2.imshow(wTitle, frame)
+                cv2.imshow(wTitle, img)
                 key = 0
                 while key != 32:  # space
                     key = cv2.waitKey(1) & 0xFF  # esc
@@ -190,13 +236,13 @@ def main(annotated: str, reviewed: str, video: str, scale: float = 2, vidreview:
                 cv2.destroyAllWindows()
 
             if video:
-                _, frame = vid.read()
+                _, img = vid.read()
                 # frame = frame[:, :, ::-1]
     if writer is not None:
         writer.release()
     if video:
         vid.release()
-    return numchanges, numcorpos
+    return numclschanges, numattrchanges, numcorcls, numcorattr
 
 
 if __name__ == '__main__':
@@ -211,7 +257,11 @@ if __name__ == '__main__':
     parser.add_argument('-k', '--keyframes', type=str, default='1-$', help='Target intervals of frames if necessary')
     parser.add_argument('-e', '--epsilon', type=float, default=0, help='The maximum permissible error of the bbox dimension')
     opt = parser.parse_args()
+    #'-a original_3-38_3-52.json -r review_3-38_3-52.json -k 1-35 -e 2 -v Cflo_troph_count_3-38_3-52.mp4 -o 1.mp4'.split())
     res = main(**vars(opt))
-    print("Number of corrected positions in fact is", res[0])
-    print("Number of corrections indeed is", res[1])
+    print(
+        (f"Corrected classes: {res[0]}\n"
+         f"Corrected attributes: {res[1]}\n"
+         f"Corrected classes in keyframes: {res[2]}\n"
+         f"Corrected attributes in keyframes: {res[3]}").format(res))
 
