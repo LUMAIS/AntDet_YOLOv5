f066daf565131b7ca2b8a1cb473ee0dd115d477c
PikachuDeveloper <shevacharmeleon@gmail.com>
2022-07-07 11:25:55 +0200

    data converters added

---
+++ dataConverters.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+:Description: converters between YOLO, Lablebox import and Labelbox export data formats.
+
+:Author: (c) Valentyna Pryhodiuk <vpryhodiuk@lumais.com>
+:Date: 2022-07-06
+"""
+import json
+import os
+import uuid
+import cv2
+
+from collections import defaultdict
+
+schema_lookup = {0: 'ckty9dfw44f8h0y9w0cnje3yr', 1: 'ckty9dfw44f8j0y9w9jgo7zx4',
+                 2: 'ckty9dfw54f8l0y9wb6ig7vu4', 3: 'ckty9dfw54f8n0y9wcrb65ies',
+                 4: 'ckty9dfw54f8p0y9w4qxygs9m', 5: 'ckty9dfw64f8r0y9wcu08h7ca',
+                 6: 'ckty9dfw64f8t0y9wb3lv6svx', 7: 'ckty9dfw64f8v0y9wfnwl2fht', 8: 'ckty9dfw64f8x0y9w4kske8vf'}
+
+class2id = {"ant": 0,
+            "ant-head": 1,
+            "trophallaxis-ant": 2,
+            "larva": 3,
+            "trophallaxis-larva": 4,
+            "food-noise": 5,
+            "pupa": 6,
+            "barcode": 7,
+            "uncategorized": 8}
+
+
+# ------------------------ YOLO -> Labelbox (new type) ------------------------
+
+def convert_yn(yolopath: str, imgpath: str, datarow_id: str, startframe: int = 1, lastframe: int = None) -> list:
+    """
+    Creates annotations using txt file with model predictionsOF THE FIRST FRAME and map it
+     to all frames according to Labelbox Import style from startframe to lastframe.
+
+    Args:
+        yolopath (str): path to the txt annotation file of the YOLO format
+        imgpath (str): path to the annotated picture
+        datarow_id (str): id of the data_row to add this annotation to
+        startframe (int): the number of the frame, which was annotated in a current file
+        lastframe (int): the number of the last frame, which was annotated in a current file
+
+    Returns:
+        json representation of a bounding box
+    """
+    annotations = []
+    img = cv2.imread(imgpath)
+    height, width = img.shape[:2]
+    with open(yolopath, 'r') as f:
+        for line in f:
+            cls, center_x, center_y, w, h = line.split()  # tuple(map(lambda x: int(x), obj.split()))
+            center_x, w = float(center_x) * width, float(w) * width
+            center_y, h = float(center_y) * height, float(h) * height
+            # cls, center_x, center_y, w, h = tuple(map(lambda x: float(x), line.split()))
+            part = {"uuid": str(uuid.uuid4()),
+                    "schemaId": schema_lookup[int(cls)],
+                    "dataRow": {
+                        "id": datarow_id
+                    },
+                    "segments": [
+                        {
+                            "keyframes": [
+                                {
+                                    "frame": startframe,
+                                    "bbox": {
+                                        "top": center_y - h / 2,
+                                        "left": center_x - w / 2,
+                                        "height": h,
+                                        "width": w
+                                    }
+                                }]
+                        }]
+                    }
+            if lastframe:
+                part["segments"][0]['keyframes'] += [{
+                    "frame": lastframe,
+                    "bbox": {
+                        "top": center_y - h / 2,
+                        "left": center_x - w / 2,
+                        "height": h,
+                        "width": w
+                    }
+                }]
+            annotations.append(part)
+
+    return annotations
+
+
+# ------------------------ YOLO -> Labelbox (old type) ------------------------
+
+# NOT the case when we have featureIds
+def convert_yo(dirpath: str, imgpath: str) -> list:
+    """
+    Creates annotations using YOLO created annotations to convert them to the old Labelbox format.
+    (to use orbAnalysis app to track id's)
+
+    Args:
+        dirpath (str): path to a directory with txt annotation files (YOLO format)
+        imgpath (str): path to the annotated picture (to take dimensions)
+
+    Returns:
+        json representation of a whole video
+    """
+    classes = {val: key for key, val in class2id.items()}
+    cur = os.getcwd()
+    os.chdir(dirpath)
+    filenames = next(os.walk(dirpath), (None, None, []))[2]
+    annotations = []
+    img = cv2.imread(imgpath)
+    height, width = img.shape[:2]
+    filenames.sort()
+    print(filenames)
+
+    for file in filenames:
+        with open(file, 'r') as f:
+            framenum = int(file.rstrip('.txt').split('_')[-1])
+            part = {"frameNumber": framenum, "objects": []}
+            for line in f:
+                cls, center_x, center_y, w, h = line.split()  # tuple(map(lambda x: int(x), obj.split()))
+                center_x, w = float(center_x) * width, float(w) * width
+                center_y, h = float(center_y) * height, float(h) * height
+                # cls, center_x, center_y, w, h = tuple(map(lambda x: float(x), line.split()))
+                part["objects"].append({"schemaId": schema_lookup[int(cls)],
+                                        "title": classes[int(cls)],
+                                        "value": classes[int(cls)],
+                                        "bbox": {
+                                            "top": center_y - h / 2,
+                                            "left": center_x - w / 2,
+                                            "height": h,
+                                            "width": w
+                                        },
+                                        "classifications": []
+                                        })
+            annotations.append(part)
+
+    return annotations
+
+
+# ------------------------ Labelbox (new type) -> Labelbox (old type) ------------------------
+
+def convert_no(filepath: str) -> list:
+    """
+    Creates annotations using YOLO created annotations to convert them to the old Labelbox format.
+
+    Args:
+        filepath (str): path to a file (Labelbox new)
+
+    Returns:
+        json representation of a whole video
+    """
+    res = defaultdict(list)  # {"frameNumber": i, "objects": []} for i in range(1, )]
+    schema2cls = {val: key for key, val in class2id.items()}  # schema_lookup.items()}
+    schema2cls = {schema_lookup[key]: val for key, val in schema2cls.copy().items()}
+
+    with open(filepath, 'r') as file:
+        annotations = json.load(file)
+
+    def scale(bbox1, bbox2, l1, l2):
+        bbox = dict()
+        for k in bbox2:
+            bbox[k] = (float(bbox1[k]) * l1 + float(bbox2[k]) * l2) / (l1 + l2)
+        return bbox
+
+    for obj in annotations:
+        featureId = str(uuid.uuid4())
+        for an in obj["segments"]:
+            keyframe1 = an['keyframes'][0]
+            keyframe2 = keyframe1 if len(an['keyframes']) == 1 else an['keyframes'][1]
+            fstart = keyframe1['frame']
+            fend = fstart if len(an['keyframes']) == 1 else keyframe2['frame']
+            for i in range(fstart, fend + 1):
+                res[i] += [
+                    {
+                        'featureId': featureId,
+                        'bbox': scale(keyframe1['bbox'], keyframe2['bbox'], fend - i + 1, i - fstart),
+                        'schemaId': obj['schemaId'],
+                        'title': schema2cls[obj['schemaId']],
+                        'value': schema2cls[obj['schemaId']]
+                    }
+                ]
+
+    result = [{"frameNumber": k, "objects": v} for k, v in dict(res).items()]
+
+    return result
--- orbAnalysis.py
+++ orbAnalysis.py
@@ -29,8 +29,9 @@ def visualize_bbox(image: np.ndarray, tool, bold=False, dashed=False, thickness:
     start = (int(tool['bbox']["left"]), int(tool['bbox']["top"]))
     end = (int(tool['bbox']["left"] + tool['bbox']["width"]),
            int(tool['bbox']["top"] + tool['bbox']["height"]))
-    h = tool['color'].lstrip('#')
-    color = tuple(int(h[i:i + 2], 16) for i in (4, 2, 0))  # BGR
+    # h = tool['color'].lstrip('#')
+    # color = tuple(int(h[i:i + 2], 16) for i in (4, 2, 0))  # BGR
+    color = (0, 0, 1)
 
     k = 1 if not bold else 2
     if not dashed:
@@ -38,11 +39,11 @@ def visualize_bbox(image: np.ndarray, tool, bold=False, dashed=False, thickness:
     else:
         dashrect(image, start, end, color, thickness * k, 'dashed')
 
-    if tool['id'][:2] != "ah" and tool['id'][0] == "a":
-        cv2.putText(image, tool['id'], (start[0], end[1]),
+    if tool['value'] == "ant":  # tool['id'][:2] != "ah" and tool['id'][0] == "a":
+        cv2.putText(image, tool['featureId'], (start[0], end[1]),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.8, color, 1 * k)
     else:
-        cv2.putText(image, tool['id'], (start[0], start[1] - 3),
+        cv2.putText(image, tool['featureId'], (start[0], start[1] - 3),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.8, color, 1 * k)
 
     return image
@@ -62,7 +63,8 @@ class App:
         _, self.fframe = self.vid.read()
         _, self.nframe = self.vid.read()
 
-        self.notClear = {'2': ['a2', 'ah5']}    # will be changed after another script will become completed
+        # self.notClear = {'2': ['a2', 'ah5']}    # will be changed after another script will become completed
+        self.notClear = {}
         self.mode = '1'
         self.h, self.w = self.fframe.shape[:2]
         self.windowName = 'image'
@@ -147,7 +149,7 @@ class App:
                 end = (int(obj['bbox']["left"] + obj['bbox']["width"]),
                        int(obj['bbox']["top"] + obj['bbox']["height"]))
                 if start[0] < x - self.w * k < end[0] and start[1] < y < end[1]:
-                    ids.append(obj['id'])
+                    ids.append(obj['featureId'])
         else:
             k = 1 if y > self.h else 0
             for obj in self.file[self.trackerPos + k]['objects']:
@@ -155,7 +157,7 @@ class App:
                 end = (int(obj['bbox']["left"] + obj['bbox']["width"]),
                        int(obj['bbox']["top"] + obj['bbox']["height"]))
                 if start[0] < x < end[0] and start[1] < y - self.h * k < end[1]:
-                    ids.append(obj['id'])
+                    ids.append(obj['featureId'])
 
         if event == cv2.EVENT_LBUTTONDOWN:
             if not self.roi:
@@ -174,16 +176,16 @@ class App:
                         changed = {'old': -1,
                                    'new': -1}
                         for i, obj in enumerate(self.file[self.trackerPos + 1]['objects']):
-                            if obj['id'] == id:
+                            if obj['featureId'] == id:
                                 changed['old'] = i
-                            elif obj['id'] == newId:
+                            elif obj['featureId'] == newId:
                                 changed['new'] = i
                         if changed['old'] != -1:
                             if changed['new'] != -1:
-                                self.file[j]['objects'][changed['old']]['id'], \
-                                self.file[j]['objects'][changed['new']]['id'] = newId, id
+                                self.file[j]['objects'][changed['old']]['featureId'], \
+                                self.file[j]['objects'][changed['new']]['featureId'] = newId, id
                             else:
-                                self.file[j]['objects'][changed['old']]['id'] = newId
+                                self.file[j]['objects'][changed['old']]['featureId'] = newId
 
                 except ValueError:
                     pass
@@ -238,11 +240,11 @@ class App:
                         cv2.FONT_HERSHEY_SIMPLEX, 1.2, (255, 0, 0), 4)
 
             for obj in self.file[self.trackerPos + i]['objects']:
-                bold = True if obj['id'] in ids else False
+                bold = True if obj['featureId'] in ids else False
                 dashed = False
                 if i == 1:
                     try:
-                        dashed = True if obj['id'] in self.notClear[str(self.trackerPos + 2)] else False
+                        dashed = True if obj['featureId'] in self.notClear[str(self.trackerPos + 2)] else False
                     except KeyError:
                         pass
                 img[i] = visualize_bbox(img[i], obj, thickness=rt, bold=bold, dashed=dashed)
@@ -279,8 +281,8 @@ class App:
 
                 if x1 < mid[0] < x2 and y1 < mid[1] < y2:
                     for n_obj in self.file[self.trackerPos + 1]['objects']:
-                        if p_obj['id'] == n_obj['id']:
-                            bold = True if p_obj['id'] in ids else False
+                        if p_obj['featureId'] == n_obj['featureId']:
+                            bold = True if p_obj['featureId'] in ids else False
                             if (self.mode == '2' and bold) or self.mode == '1':
                                 # obj_in_area += 1 if self.mode == '2' and bold else 0
                                 img = self.visualize_line(img, p_obj, n_obj, thickness=rt, bold=bold)
@@ -311,8 +313,9 @@ class App:
             end = (int(n_obj['bbox']["left"] + n_obj['bbox']["width"] / 2),
                    int(n_obj['bbox']["top"] + n_obj['bbox']["height"] / 2 + self.h))
 
-        h = p_obj['color'].lstrip('#')
-        color = tuple(int(h[i:i + 2], 16) for i in (4, 2, 0))  # BGR
+        # h = p_obj['color'].lstrip('#')
+        # color = tuple(int(h[i:i + 2], 16) for i in (4, 2, 0))  # BGR
+        color = (0, 0, 1)
 
         k = 1 if not bold else 2
         cv2.line(image, start, end, color=color, thickness=thickness * k)
@@ -349,7 +352,8 @@ if __name__ == '__main__':
     opt = parser.parse_args()
     # using test-parameters
     # opt = parser.parse_args("-vid /home/valia/AntVideos/Cflo_troph_count_3-38_3-52.mp4 "
-    #                         "-a /home/valia/AntVideos/Cflo_troph_count_3-38_3-52_withId.json".split())
+    #                         "-a /home/valia/Downloads/lo.json".split())
+                            # "-a /home/valia/AntVideos/Cflo_troph_count_3-38_3-52_withId.json".split())
     w, h = opt.wsize.split('x')
     flag = True if opt.horizontal else False
     flag = True if not opt.horizontal and not opt.vertical else flag
